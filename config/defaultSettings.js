c9d35f0018de284c2f7c1aefb021c54376b8d520
wukai <wukai1981@gmail.com>
2020-04-19 08:59:51 +0800

    init

---
+++ config/defaultSettings.js
@@ -0,0 +1,18 @@
+export default {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    // locale: true,
+    locale: false
+  },
+  title: 'Three Years',
+  pwa: false,
+  iconfontUrl: '',
+};
