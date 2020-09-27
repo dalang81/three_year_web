22fef08b3471609f2058d7d30e2c40f1535be3b5
kaka <wukai1981@gmail.com>
2020-09-27 16:12:35 +0800

    for UpdateForm.jsx

--- src/pages/userManagement/components/CreateForm.jsx
+++ src/pages/userManagement/components/CreateForm.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Modal } from 'antd';
+import {Modal} from 'antd';
 
 const CreateForm = (props) => {
-  const { modalVisible, onCancel } = props;
+  const {modalVisible, onCancel} = props;
   return (
     <Modal
       destroyOnClose
-      title="新建规则"
+      title="新建用户"
       visible={modalVisible}
       onCancel={() => onCancel()}
       footer={null}
