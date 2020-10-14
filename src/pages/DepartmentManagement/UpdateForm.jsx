3873268d97634f0f64ba5df9a2849dc19989b8d2
kaka <wukai1981@gmail.com>
2020-10-14 16:47:00 +0800

    userManagement update

--- src/pages/DepartmentManagement/UpdateForm.jsx
+++ src/pages/DepartmentManagement/UpdateForm.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
-import {Modal} from 'antd';
+import {Modal, Form, Input} from 'antd';
 
-
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
 const UpdateForm = (props) => {
-  const {modalVisible, onCancel, values} = props;
-  console.log(' UpdateForm values ', values);
+  const {modalVisible, onCancel, values, onFinish} = props;
   return (
     <Modal
       destroyOnClose
       title="编辑部门"
       visible={modalVisible}
+      onOk={v => onFinish && onFinish(v)}
       onCancel={() => onCancel()}
-      footer={null}
+      // footer={null}
     >
-      {props.children}
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{...values}}
+        // onFinish={v => onFinish && onFinish(v)}
+        //  onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="全名"
+          name="fullname"
+          rules={[
+            {
+              required: true,
+              message: '请输入部门全名!',
+            },
+          ]}
+        >
+          <Input/>
+        </Form.Item>
+
+        <Form.Item
+          label="描述"
+          name="description"
+        >
+          <Input.TextArea/>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
